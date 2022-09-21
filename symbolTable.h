b8c644d232598031243a688eec7bd93b65176a0b
Ty Hunt <ty@Ty.localdomain>
2022-09-21 13:45:58 -0700

    Started work on Symbol Table

--- symbolTable.h
+++ symbolTable.h
@@ -1,6 +1,24 @@
 // todo Attributes: ID, Add Item, Kind (function or variable), Type (int, char, etc)
+struct Entry {
+    int itemID;
+    char itemName[50];
+    char itemKind[8];
+    int arrayLength;
+    char scope[50];
+}
+
+struct entry tableItems[100];
+int tableIndex = 0;
+
 // todo Function to add Items to a list
-// todo Function to and see if the item is found.
+void addItem(char itemName[50], char itemKind[8], int arrayLength, char scope[50]) {}
+
+// todo - Function to and see if the item is found
+void found(char itemName[50], char scope[50]) {}
+
+// todo - Print out our symbol table to the screen
+void print() {}
+
 // ? What is the scope? is it (Global or Local)
 // ? How much of your program can I borrow? 
 // ? Should we implement this using a linked list instead? How do you want us to go about this?
